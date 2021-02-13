ccb9b60624d4c80157f6b4bee279e4c957a4af4b
mirandadayadkins <mirandaday16@gmail.com>
2021-02-13 15:19:51 -0500

    added template code to user.service.client.js

---
+++ src/main/webapp/services/user.service.client.js
@@ -0,0 +1,24 @@
+function AdminUserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001367581/users';
+    var self = this;
+    function createUser(user) {
+        // TODO: Implement
+    }
+    function findAllUsers() {
+        // TODO: Implement
+    }
+    function findUserById(userId) {
+        // TODO: Implement
+    }
+    function updateUser(userId, user) {
+        // TODO: Implement
+    }
+    function deleteUser(userId) {
+        // TODO: Implement
+    }
+}
