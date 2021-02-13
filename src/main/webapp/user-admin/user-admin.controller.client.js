7da404c11f77f7a2737ad9a80b3c0788d00c3a9f
mirandadayadkins <mirandaday16@gmail.com>
2021-02-13 15:15:52 -0500

    user admin controller added

---
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -0,0 +1,65 @@
+// (function () {
+    const users = [
+        {id: 123, username: 'alice', firstName: 'Alice', lastName: 'Smith', role: "FACULTY"},
+        {id: 456, username: 'bailey', firstName: 'Bailey', lastName: 'Taylor', role: "STUDENT"},
+        {id: 789, username: 'charlie', firstName: 'Charlie', lastName: 'Reynolds', role: "STUDENT"},
+        {id: 135, username: 'dan', firstName: 'Dan', lastName: 'Green', role: "FACULTY"}
+    ];
+
+
+    var rowTemplate;
+    var tbody;
+
+    jQuery(main);
+
+    function main() {
+        rowTemplate = jQuery('.mda-template');
+        tbody = jQuery('tbody');
+        renderUsers(users);
+    }
+
+    function renderUsers(users) {
+        for (var u in users) {
+            const user = users[u]
+            const rowClone = rowTemplate.clone();
+            rowClone.removeClass('mda-hidden');
+            rowClone.find('.mda-username').html(user.username)
+            rowClone.find('.mda-first-name').html(user.firstName)
+            rowClone.find('.mda-last-name').html(user.lastName)
+            rowClone.find('.mda-role').html(user.role)
+            tbody.append(rowClone);
+        }
+    }
+
+    // var $usernameFld, $passwordFld;
+    // var $firstNameFld, $lastNameFld, $roleFld;
+    // var $removeBtn, $editBtn, $createBtn;
+    // var $userRowTemplate, $tbody;
+    // var userService = new AdminUserServiceClient();
+    // $(main);
+    //
+    // function main() {
+    //     // TODO: Implement
+    // }
+    // function createUser() {
+    //     // TODO: Implement
+    // }
+    // function deleteUser() {
+    //     // TODO: Implement
+    // }
+    // function selectUser() {
+    //     // TODO: Implement
+    // }
+    // function updateUser() {
+    //     // TODO: Implement
+    // }
+    // function renderUsers(users) {
+    //     // TODO: Implement
+    // }
+    // function findAllUsers() { // optional - might not need this
+    //     // TODO: Implement
+    // }
+    // function findUserById() { // optional - might not need this
+    //     // TODO: Implement
+    // }
+// })();
