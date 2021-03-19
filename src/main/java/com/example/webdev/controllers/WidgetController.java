b1e48264ed4a859c4498bf0765d7a90fd36023ef
mirandadayadkins <mirandaday16@gmail.com>
2021-03-19 13:54:37 -0400

    added cross origin to widget controller

--- src/main/java/com/example/webdev/controllers/WidgetController.java
+++ src/main/java/com/example/webdev/controllers/WidgetController.java
@@ -3,6 +3,7 @@ package com.example.webdev.controllers;
 import com.example.webdev.models.Widget;
 import com.example.webdev.services.WidgetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class WidgetController {
     @Autowired
     WidgetService service;
