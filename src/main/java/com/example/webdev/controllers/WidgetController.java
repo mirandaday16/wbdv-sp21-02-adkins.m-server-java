efedce7a26f81faee808080405c60ad4c52bd576
mirandadayadkins <mirandaday16@gmail.com>
2021-03-20 12:51:06 -0400

    create widget functionality added to service and controller

--- src/main/java/com/example/webdev/controllers/WidgetController.java
+++ src/main/java/com/example/webdev/controllers/WidgetController.java
@@ -3,11 +3,9 @@ package com.example.webdev.controllers;
 import com.example.webdev.models.Widget;
 import com.example.webdev.services.WidgetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -25,6 +23,14 @@ public class WidgetController {
     public List<Widget> findWidgetsForTopic(
             @PathVariable("topicId") String topicId) {
         return service.findWidgetsForTopic(topicId);
+    }
+
+    @PostMapping("/api/topics/{topicId}/widgets")
+    public  Widget createWidgetForTopic(
+            @PathVariable("topicId") String topicId,
+            @RequestBody Widget widget
+    ) {
+        return service.createWidget(topicId, widget);
     }
 
 }
--- src/main/java/com/example/webdev/services/WidgetService.java
+++ src/main/java/com/example/webdev/services/WidgetService.java
@@ -4,13 +4,14 @@ import com.example.webdev.models.Widget;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class WidgetService {
     private List<Widget> widgets = new ArrayList<Widget>();
     {
-        Widget widget1 = new Widget(123L, "ABC1", "HEADING", 1, "Widgets for Topic 1!");
+        Widget widget1 = new Widget(123L, "6047c76bf10b760017274b61", "HEADING", 1, "Widgets for Topic 1!");
         Widget widget2 = new Widget(234L, "ABC1", "PARAGRAPH", 1, "Lorem Ipsum 1");
         Widget widget3 = new Widget(345L, "ABC2", "HEADING", 2, "Widgets for Topic 2!");
         Widget widget4 = new Widget(456L, "ABC2", "PARAGRAPH", 1, "Lorem Ipsum 2");
@@ -35,6 +36,13 @@ public class WidgetService {
             }
         }
         return widgetList;
+    }
+
+    public Widget createWidget(String topicId, Widget widget) {
+        widget.setTopicId(topicId);
+        widget.setId((new Date()).getTime());
+        widgets.add(widget);
+        return widget;
     }
 
 
